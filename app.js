a1e1f63c3a8c7e34ff44026a46077417901a607e
Jordi2233 <User123.Name456@protonmail.com>
2022-04-22 15:23:16 +0200

    google lvl auth

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const session = require('express-session');
+const session = require("express-session");
 const passport = require("passport");
+const passportLocal = require("passport-local")
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require('mongoose-findorcreate');
+const findOrCreate = require("mongoose-findorcreate");
 
 
 const app = express();
@@ -78,15 +79,15 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user.id);
-  });
+});
 
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
     });
-  });
+});
 
 
 // *****************************************************************************
@@ -126,24 +127,11 @@ const run = async () => {
                 res.render("login");
             })
 
-            .post(passport.authenticate("local", {
-                failureRedirect: "/login"
-            }), (req, res) => {
-
-                const user = new User({
-                    username: req.body.username,
-                    password: req.body.password
-                });
-                req.login(user, (err) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        res.redirect("/secrets");
-
-                    }
-                });
-
-            });
+            .post(passport.authenticate('local', { 
+                successRedirect: '/secrets',
+                failureRedirect: '/login' 
+                })
+);
 
 
         // Register route
@@ -199,3 +187,7 @@ const run = async () => {
 }
 
 run().catch(console.dir);
+
+
+
+
